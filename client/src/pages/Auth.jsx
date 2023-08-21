abbbedb8b9f4fe5fb61c54fc5d607120fdd4bfe3
DivineDemon <supame123@gmail.com>
2023-08-21 23:51:51 +0500

    :lipstick: :hammer: :sparkles:

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,18 +1,21 @@
 import { useDispatch } from "react-redux";
-import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
+import Loading from "../components/Loading";
 import { register, login } from "../store/api";
 import FormGroup from "../components/FormGroup";
-import { isAuthenticated } from "../utils/helpers";
+import { imageToBase64, isAuthenticated } from "../utils/helpers";
 
 const Auth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [text, setText] = useState("");
   const [email, setEmail] = useState("");
+  const [image, setImage] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [isExisting, setIsExisting] = useState(false);
 
   useEffect(() => {
@@ -20,6 +23,29 @@ const Auth = () => {
       setText(document.getElementById("username_or_email").value);
     }
   }, [isExisting]);
+
+  const triggerUpload = () => {
+    document.getElementById("imageUpload").click();
+  };
+
+  const handleImageUpload = async (e) => {
+    setLoading(true);
+    const base64 = await imageToBase64(e.target.files[0]);
+
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ image: base64 }),
+    });
+
+    const data = await response.json();
+    if (data.url) {
+      setLoading(false);
+      setImage(data.url);
+    }
+  };
 
   const submitAuthForm = async (e) => {
     e.preventDefault();
@@ -36,7 +62,7 @@ const Auth = () => {
         navigate("/");
       }
     } else {
-      dispatch(register({ username, email, password }));
+      dispatch(register({ username, email, password, image }));
     }
   };
 
@@ -87,6 +113,42 @@ const Auth = () => {
           label="Password"
           setPassword={setPassword}
         />
+        {!isExisting && (
+          <>
+            <div className="w-full h-full rounded-md flex flex-col items-start justify-start space-y-3">
+              <label className="text-sm font-semibold" htmlFor="image">
+                Profile Image
+              </label>
+              <input
+                type="file"
+                name="image"
+                id="imageUpload"
+                className="hidden"
+                onChange={(e) => handleImageUpload(e)}
+              />
+              <button
+                type="button"
+                onClick={triggerUpload}
+                className={
+                  image ? "hidden" : "px-5 py-3 text-white font-semibold rounded-lg bg-black"
+                }
+              >
+                Upload Image
+              </button>
+            </div>
+            {loading ? (
+              <Loading />
+            ) : (
+              <div className="w-full flex items-center justify-center">
+                <img
+                  src={image}
+                  alt="profile"
+                  className={image ? "w-32 h-32 rounded-full" : "hidden"}
+                />
+              </div>
+            )}
+          </>
+        )}
         <span
           onClick={() => setIsExisting((prevState) => !prevState)}
           className="text-center text-sm font-semibold text-blue-500 underline cursor-pointer"
@@ -95,7 +157,10 @@ const Auth = () => {
             ? "Already have an account! Login."
             : "Don't Have and Account? Register!"}
         </span>
-        <button className="px-5 py-3 bg-black rounded-lg text-white text-lg font-semibold hover:bg-white hover:border hover:border-black hover:text-black transition delay-150 ease-in-out">
+        <button
+          type="submit"
+          className="px-5 py-3 bg-black rounded-lg text-white text-lg font-semibold hover:bg-white hover:border hover:border-black hover:text-black transition delay-150 ease-in-out"
+        >
           Let's Go!
         </button>
       </form>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,16 +2,36 @@ const uploadProfile = async (req, res) => {
   const formData = new FormData();
   formData.append("image", req.body.image);
 
-  const response = await fetch(
-    `https://api.imgbb.com/1/upload?key=${process.env.IMG_BB}`,
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
+  try {
+    const response = await fetch(
+      `https://api.imgbb.com/1/upload?key=${process.env.IMG_BB}`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
-  const data = await response.json();
-  return data.url;
+    const data = await response.json();
+
+    if (data.data.url) {
+      return res.status(200).json({
+        success: true,
+        message: "Successfully Stored Image!",
+        url: data.data.url,
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Failed to Stored Image!",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Please Try Again!",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
