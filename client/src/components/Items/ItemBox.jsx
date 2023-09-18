b75085bebd67dd877accee26d1d971544e970fce
DivineDemon <supame123@gmail.com>
2023-09-19 00:44:57 +0500

    Design!

--- client/src/components/Items/ItemBox.jsx
+++ client/src/components/Items/ItemBox.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 const ItemBox = ({ item }) => {
   return (
-    <div className="w-[350px] text-center rounded-lg border border-yellow-500 flex flex-col items-center justify-center space-y-2">
+    <div className="w-[350px] rounded-lg border border-yellow-500 flex flex-col items-center justify-center">
       <img
         src={item.image}
         alt="item"
         className="w-full h-[50%] object-cover"
       />
-      <p className="text-xl font-bold">{item.name}</p>
-      <p className="text-sm font-semibold text-gray-400">{item.description}</p>
-      <p>
-        Rs. <span className="font-bold">{item.price}/-</span>
-      </p>
+      <div className="p-3 flex flex-col items-center justify-center text-center space-y-2">
+        <p className="text-xl font-bold">{item.name}</p>
+        <p className="text-sm font-semibold text-gray-400">
+          {item.description}
+        </p>
+        <p>
+          Rs. <span className="font-bold">{item.price}/-</span>
+        </p>
+      </div>
     </div>
   );
 };
