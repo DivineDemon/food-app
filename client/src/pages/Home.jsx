5dc542ad7cec9cd655dc587075ec946ffef8fc29
DivineDemon <supame123@gmail.com>
2023-09-18 19:56:29 +0500

    Revamping Frontend!

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-import Navbar from "../components/Navbar";
-import Banner from "../components/Banner";
-import ItemList from "../components/ItemList";
-import CategoryList from "../components/CategoryList";
-
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <Banner />
-      <CategoryList />
-      <ItemList />
-    </>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
